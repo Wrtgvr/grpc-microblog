caf526442a265f09518918c9062f1a9e6c887cbf
Wrtgvr <saldaniil123@gmail.com>
2025-05-25 15:05:56 +0700

    Added: graceful shutdown

--- cmd/user-server/main.go
+++ cmd/user-server/main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	usersapp "github.com/wrtgvr/grpc-microblog/internal/app/users"
 )
 
+var (
+	stop = make(chan os.Signal, 1)
+)
+
 func main() {
 	app := usersapp.NewApp(50051)
-	app.MustRun()
+	go app.MustRun()
 
-	fmt.Println("Server running")
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+	fmt.Println("Stopping server")
+	app.Stop()
+	fmt.Println("Server stopped")
 }

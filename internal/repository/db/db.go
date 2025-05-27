c7350a6718d10ae1c8fc9cf55bc970c2eff53552
Wrtgvr <saldaniil123@gmail.com>
2025-05-27 12:25:00 +0700

    Renamed: InitDb -> OpenDb Added: MustOpen db func.

--- internal/repository/db/db.go
+++ internal/repository/db/db.go
@@ -7,7 +7,15 @@ import (
 	"gorm.io/gorm"
 )
 
-func InitDb(dsn string) (*gorm.DB, error) {
+func MustOpenDb(dsn string) *gorm.DB {
+	db, err := OpenDb(dsn)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
+func OpenDb(dsn string) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
